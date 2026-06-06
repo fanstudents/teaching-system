835b217841086c7f90dad6d50ab9a2a80890dba0
jrtingfan <jrtingfan@gmail.com>
2026-06-06 09:24:08 +0800

    chore: 更新 webProject.js cache buster v=20260606

--- scripts/slideManager.js
+++ scripts/slideManager.js
@@ -1282,7 +1282,7 @@ export class SlideManager {
                 el.style.overflow = 'hidden';
                 requestAnimationFrame(() => {
                     const isLive = el.closest('.presentation-slide') || el.closest('.aud-interaction-wrap');
-                    import('./interactive/webProject.js?v=20260604').then(({ WebProjectGame }) => {
+                    import('./interactive/webProject.js?v=20260606').then(({ WebProjectGame }) => {
                         if (isLive) {
                             new WebProjectGame().render(el, element);
                         } else {
